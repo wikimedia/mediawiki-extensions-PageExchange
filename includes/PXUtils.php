127fe7ec89884773d0e783a03ad0865cd15a7dcf
Yaron Koren <yaron57@gmail.com>
2021-07-12 22:36:05 +0000

    Fix error display for broken page URLs (with cURL)
    Change-Id: I5637c92dca60092b338ca991b3ea0ea464de67c7

--- includes/PXUtils.php
+++ includes/PXUtils.php
@@ -46,7 +46,12 @@ class PXUtils {
 			curl_setopt( $ch, CURLOPT_RETURNTRANSFER, 1 );
 			curl_setopt( $ch, CURLOPT_URL, $url );
 			curl_setopt( $ch, CURLOPT_USERAGENT, 'request' );
-			return curl_exec( $ch );
+			$contents = curl_exec( $ch );
+			$httpCode = curl_getinfo( $ch, CURLINFO_HTTP_CODE );
+			if ( $httpCode !== 200 ) {
+				return '';
+			}
+			return $contents;
 		}
 
 		if ( method_exists( AtEase::class, 'suppressWarnings' ) ) {
