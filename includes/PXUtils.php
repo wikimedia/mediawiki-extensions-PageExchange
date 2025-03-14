2e43e1cdf07d27e78a24602f0e64e34b5811567d
Yaron Koren <yaron57@gmail.com>
2025-03-14 13:56:10 +0000

    Further removal of pre-MW 1.40 compatibility
    Change-Id: Ifcb7c103f44cbc5446705acf401d2f345e2bd1f8

--- includes/PXUtils.php
+++ includes/PXUtils.php
@@ -88,13 +88,9 @@ class PXUtils {
 	 * @return Database
 	 */
 	public static function getReadDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getReplicaDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getReplicaDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_REPLICA );
-		}
+		return MediaWikiServices::getInstance()
+			->getDBLoadBalancerFactory()
+			->getReplicaDatabase();
 	}
 
 	/**
@@ -103,12 +99,8 @@ class PXUtils {
 	 * @return Database
 	 */
 	public static function getWriteDB() {
-		$lbFactory = MediaWikiServices::getInstance()->getDBLoadBalancerFactory();
-		if ( method_exists( $lbFactory, 'getPrimaryDatabase' ) ) {
-			// MW 1.40+
-			return $lbFactory->getPrimaryDatabase();
-		} else {
-			return $lbFactory->getMainLB()->getConnection( DB_PRIMARY );
-		}
+		return MediaWikiServices::getInstance()
+			->getDBLoadBalancerFactory()
+			->getPrimaryDatabase();
 	}
 }
